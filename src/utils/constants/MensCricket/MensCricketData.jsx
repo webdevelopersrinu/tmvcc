763bae4a5b6303c43de21220efe85c2b05cb6e46
srinu <webdeveloper.srinu9@gmail.com>
2024-11-30 17:50:48 +0530

    add google font

--- src/utils/constants/MensCricket/MensCricketData.jsx
+++ src/utils/constants/MensCricket/MensCricketData.jsx
@@ -7,22 +7,22 @@ import MensCricket2 from "./../../../assets/teams/2.png";
 export const CRICKET_DATA = [
   {
     name: "Men's Cricket 1's XI",
-    data: "Team Sheet for 31/08/2024:",
+    data: "Team Sheet for 30/11/2024:",
     img: MensCricket1,
   },
   {
     name: "Men's Cricket 2's XI",
-    data: "Team Sheet for 31/08/2024:",
+    data: "Team Sheet for 30/11/2024:",
     img: MensCricket2,
   },
   {
     name: "Men's Cricket 3's XI",
-    data: "Team Sheet for 31/08/2024:",
+    data: "Team Sheet for 30/11/2024:",
     img: MensCricket3,
   },
   {
     name: "Men's Cricket 4's XI",
-    data: "Team Sheet for 31/08/2024:",
+    data: "Team Sheet for 30/11/2024:",
     img: MensCricket4,
   },
 ];
