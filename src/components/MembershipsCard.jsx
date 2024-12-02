9b02e2a9f55d633ede31e9eb18033e281bc2f7f1
srinu <webdeveloper.srinu9@gmail.com>
2024-12-02 05:50:17 +0530

    add payment link

--- src/components/MembershipsCard.jsx
+++ src/components/MembershipsCard.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 function MembershipsCard({ cardData }) {
   const { name, link, price, renewalDate, startDate } = cardData;
   return (
-    <div className="bg-[#F6F6F6] my-4 border border-t-primary-color border-t-4 flex-col py-3
-     sm:flex-row flex items-center justify-between px-4 shadow-xl">
+    <div
+      className="bg-[#F6F6F6] my-4 border border-t-primary-color border-t-4 flex-col py-3
+     sm:flex-row flex items-center justify-between px-4 shadow-xl"
+    >
       <Link to={link}>
-        <div >
+        <div>
           <h1 className="text-3xl font-bold py-4">{name}</h1>
           <div className="text-xs sm:text-lg  flex items-center sm:gap-2 flex-col py-2">
             <p>Start Date: {startDate}</p>
@@ -17,9 +19,11 @@ function MembershipsCard({ cardData }) {
       </Link>
       <div className="flex items-center gap-3">
         <p className="text-xl font-semibold">{price}</p>
-        <button className="py-3 px-2 rounded-lg bg-primary-color text-white font-bold ">
-          Purchase
-        </button>
+        <a href="https://pay.gocardless.com/AL0015FYG90G8T">
+          <button className="py-3 px-2 rounded-lg bg-primary-color text-white font-bold ">
+            Purchase
+          </button>
+        </a>
       </div>
     </div>
   );
