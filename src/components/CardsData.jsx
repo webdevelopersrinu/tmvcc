dfc6dba835d87f28e9f6f84f53835678706a1dc9
srinu <webdeveloper.srinu9@gmail.com>
2024-12-04 18:36:39 +0530

    change card size in moblie screen

--- src/components/CardsData.jsx
+++ src/components/CardsData.jsx
@@ -5,7 +5,7 @@ function CardsData({ cardData }) {
   const { img, title, link } = cardData;
   return (
     <div className="w-5/6 md:w-7/12 gap-3  lg:w-3/12 p-5 lg:block flex items-center justify-start text-white">
-      <div className=" w-7/12  sm:w-full  border-white border-2 hover:border-none">
+      <div className=" w-4/12  sm:w-full  border-white border-2 hover:border-none">
         <Link to={link}>
           <img src={img} alt="card image" className="w-full" />
         </Link>
